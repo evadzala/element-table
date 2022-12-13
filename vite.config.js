46d577488ea3070826c42633f476ce6f75ce8f7b
peterchiu <evadzala@gmail.com>
2022-12-13 14:47:08 +0800

    修改路徑

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/element-table/'
-    : '/',
+  publicPath: '/element-table/',
   plugins: [vue()]
 })
